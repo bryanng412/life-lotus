6167af411e5c20102cf07ccc20794b4e9caf43cc
bryanng <bryanng412@gmail.com>
2025-02-25 23:18:49 -0800

    add absolute path imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [
     react(),
     VitePWA({
