875c84d2003f42934db55076093f11c6f3b5e3b4
bryanng <bryanng412@gmail.com>
2025-03-03 22:10:59 -0800

    test

--- src/components/views/ChoosePlayer.tsx
+++ src/components/views/ChoosePlayer.tsx
@@ -2,8 +2,19 @@ import { Button } from '@/components/ui/button'
 import { useBoundStore } from '@/lib/store/boundStore'
 import { View } from '@/lib/store/viewSlice'
 import { useGesture } from '@use-gesture/react'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Circle } from 'lucide-react'
 import { useState } from 'react'
+
+const CircleColors = [
+  'oklch(0.646 0.222 41.116)',
+  'oklch(0.6 0.118 184.704)',
+  'oklch(0.398 0.07 227.392)',
+  'oklch(0.828 0.189 84.429)',
+  'oklch(0.769 0.188 70.08)',
+  'oklch(0.627 0.265 303.9)',
+]
+
+const CircleDiameter = 120
 
 const ChoosePlayer = () => {
   const { setView, previousView } = useBoundStore()
@@ -64,34 +75,46 @@ const ChoosePlayer = () => {
         {...bind()}
         className="bg-muted absolute top-0 left-0 h-screen w-screen touch-none overflow-hidden bg-[linear-gradient(to_right,var(--color-muted-foreground)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-muted-foreground)_1px,transparent_1px)] bg-[size:32px_32px] opacity-20"
       />
-      <p>Touch data</p>
-      <pre>{JSON.stringify(touches, null, 2)}</pre>
-      <>
-        <Button
-          size="lg"
-          onClick={onBackClick}
-          className="bg-border text-muted-foreground hover:bg-muted absolute top-4 left-4 hover:opacity-90 active:scale-[.98]"
-        >
-          <ArrowLeft />
-        </Button>
-        <div className="absolute top-1/4 left-1/2 flex -translate-x-1/2 transform flex-col items-center justify-center gap-3">
-          <h1 className="text-center text-3xl font-bold select-none">
-            Choose a Player
-          </h1>
-          <p className="text-center select-none">
-            Each player holds a finger on the screen. After three seconds, one
-            is chosen to go first!
-          </p>
-          <div className="align-center flex justify-center gap-2">
-            <Button
-              size="lg"
-              className="bg-muted-foreground text-primary-foreground hover:bg-muted-foreground select-none hover:opacity-90 active:scale-[.98]"
-            >
-              Skip
-            </Button>
+      {touches.map(({ id, x, y }) => (
+        <Circle
+          size={CircleDiameter}
+          key={id}
+          style={{
+            color: CircleColors[id],
+            position: 'absolute',
+            top: y - CircleDiameter / 2,
+            left: x - CircleDiameter / 2,
+          }}
+        />
+      ))}
+      {touches.length === 0 && (
+        <>
+          <Button
+            size="lg"
+            onClick={onBackClick}
+            className="bg-border text-muted-foreground hover:bg-muted absolute top-4 left-4 hover:opacity-90 active:scale-[.98]"
+          >
+            <ArrowLeft />
+          </Button>
+          <div className="absolute top-1/4 left-1/2 flex -translate-x-1/2 transform flex-col items-center justify-center gap-3">
+            <h1 className="text-center text-3xl font-bold select-none">
+              Choose a Player
+            </h1>
+            <p className="text-center select-none">
+              Each player holds a finger on the screen. After three seconds, one
+              is chosen to go first!
+            </p>
+            <div className="align-center flex justify-center gap-2">
+              <Button
+                size="lg"
+                className="bg-muted-foreground text-primary-foreground hover:bg-muted-foreground select-none hover:opacity-90 active:scale-[.98]"
+              >
+                Skip
+              </Button>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   )
 }
