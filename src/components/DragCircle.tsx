84bbdd99262c24f01810f0570361efe140d91c1a
bryanng <bryanng412@gmail.com>
2025-03-03 17:01:43 -0800

    test

--- src/components/DragCircle.tsx
+++ src/components/DragCircle.tsx
@@ -2,35 +2,40 @@ import { animated, useSpring } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 import { Circle } from 'lucide-react'
 
-const CircleColors = [
-  'oklch(0.646 0.222 41.116)',
-  'oklch(0.6 0.118 184.704)',
-  'oklch(0.398 0.07 227.392)',
-  'oklch(0.828 0.189 84.429)',
-  'oklch(0.769 0.188 70.08)',
-  'oklch(0.627 0.265 303.9)',
-]
+// const CircleColors = [
+//   'oklch(0.646 0.222 41.116)',
+//   'oklch(0.6 0.118 184.704)',
+//   'oklch(0.398 0.07 227.392)',
+//   'oklch(0.828 0.189 84.429)',
+//   'oklch(0.769 0.188 70.08)',
+//   'oklch(0.627 0.265 303.9)',
+// ]
 
-const CircleRadius = 120
+const CircleDiameter = 120
 const AnimatedCircle = animated(Circle)
 
-export type DragCircleProps = {
-  id: number
-  initialX: number
-  initialY: number
-}
+// export type DragCircleProps = {
+//   id: number
+//   initialX: number
+//   initialY: number
+// }
 
-const DragCircle = ({ id, initialX, initialY }: DragCircleProps) => {
-  const [{ x, y }, api] = useSpring(() => ({ x: initialX, y: initialY }))
+const DragCircle = () => {
+  const [{ x, y }, api] = useSpring(() => ({
+    x: 0,
+    y: 0,
+  }))
 
-  const bind = useDrag(async ({ offset: [dx, dy] }) => {
-    await Promise.all(api.start({ x: initialX + dx, y: initialY + dy }))
+  const bind = useDrag(({ offset: [x, y] }) => api.start({ x, y }), {
+    filterTaps: true,
+    preventDefault: true,
   })
+
   return (
     <AnimatedCircle
       {...bind()}
-      style={{ x, y, touchAction: 'none', color: CircleColors[id] }}
-      size={CircleRadius}
+      style={{ x, y, touchAction: 'none', zIndex: 99 }}
+      size={CircleDiameter}
     />
   )
 }
--- src/components/views/ChoosePlayer.tsx
+++ src/components/views/ChoosePlayer.tsx
@@ -1,29 +1,27 @@
-import DragCircle, { DragCircleProps } from '@/components/DragCircle'
+import DragCircle from '@/components/DragCircle'
 import { Button } from '@/components/ui/button'
 import { useBoundStore } from '@/lib/store/boundStore'
 import { View } from '@/lib/store/viewSlice'
 import { ArrowLeft } from 'lucide-react'
-import { TouchEventHandler, useState } from 'react'
+import { useState } from 'react'
 
 const ChoosePlayer = () => {
   const { setView, previousView } = useBoundStore()
-  const [touchPoints, setTouchPoints] = useState<DragCircleProps[]>([])
+  const [touchPoints] = useState([])
 
-  const handleStart: TouchEventHandler = event => {
-    event.preventDefault()
-    setTouchPoints(
-      Array.from(event.touches).map(touch => ({
-        id: touch.identifier,
-        initialX: touch.clientX,
-        initialY: touch.clientY,
-      }))
-    )
-  }
+  // const handleStart: TouchEventHandler = event => {
+  //   setTouchPoints(
+  //     Array.from(event.touches).map(touch => ({
+  //       id: touch.identifier,
+  //       initialX: touch.pageX,
+  //       initialY: touch.pageY,
+  //     }))
+  //   )
+  // }
 
-  const handleEnd: TouchEventHandler = event => {
-    event.preventDefault()
-    setTouchPoints([])
-  }
+  // const handleEnd: TouchEventHandler = event => {
+  //   setTouchPoints([])
+  // }
 
   const onBackClick = () => {
     if (previousView) {
@@ -35,15 +33,11 @@ const ChoosePlayer = () => {
 
   return (
     <div className="bg-muted relative h-screen w-screen overflow-hidden">
-      <div
-        onTouchStart={handleStart}
-        onTouchEnd={handleEnd}
-        onTouchCancel={handleEnd}
-        className="bg-muted absolute top-0 left-0 h-screen w-screen overflow-hidden bg-[linear-gradient(to_right,var(--color-muted-foreground)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-muted-foreground)_1px,transparent_1px)] bg-[size:32px_32px] opacity-20"
-      />
-      {touchPoints.map(touch => (
-        <DragCircle key={touch.id} {...touch} />
-      ))}
+      <div className="bg-muted absolute top-0 left-0 h-screen w-screen overflow-hidden bg-[linear-gradient(to_right,var(--color-muted-foreground)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-muted-foreground)_1px,transparent_1px)] bg-[size:32px_32px] opacity-20" />
+      <DragCircle />
+      <DragCircle />
+      <DragCircle />
+      <DragCircle />
       {touchPoints.length === 0 && (
         <>
           <Button
