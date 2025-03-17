4bb67d546cb7558cb830c12b8fc8b75204a1bb3a
bryanng <bryanng412@gmail.com>
2025-03-16 18:23:08 -0700

    animate end game button

--- src/components/EndGameButton.tsx
+++ src/components/EndGameButton.tsx
@@ -1,39 +1,41 @@
 import { Button } from '@/components/ui/button'
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@/components/ui/collapsible'
 import { useBoundStore } from '@/lib/store/boundStore'
 import { View } from '@/lib/store/viewSlice'
+import { AnimatePresence, motion } from 'motion/react'
 import { useState } from 'react'
 
 const EndGameButton = () => {
-  const { setView } = useBoundStore()
-  const endGameOnClick = () => setView(View.GameSetup)
   const [isOpen, setIsOpen] = useState(false)
+  const { setView } = useBoundStore()
+  const toggleIsOpen = () => setIsOpen(open => !open)
+  const endGameOnClick = () => setView(View.GameSetup)
 
   return (
-    <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className="flex flex-col items-center justify-center"
-    >
-      <CollapsibleTrigger asChild>
-        <Button>End Game</Button>
-      </CollapsibleTrigger>
-      <CollapsibleContent>
-        <div className="mt-4 flex flex-col items-center justify-center space-y-2">
-          <p>Are you sure?</p>
-          <div className="flex items-center justify-center space-x-2">
-            <Button onClick={() => setIsOpen(false)}>No</Button>
-            <Button onClick={endGameOnClick} variant="destructive">
-              Yes
-            </Button>
-          </div>
-        </div>
-      </CollapsibleContent>
-    </Collapsible>
+    <>
+      <Button onClick={toggleIsOpen}>End Game</Button>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ maxHeight: 0 }}
+            animate={{ maxHeight: 130 }}
+            exit={{ maxHeight: 0 }}
+            transition={{
+              type: 'tween',
+              ease: 'linear',
+            }}
+            className="mt-2 flex flex-col items-center justify-center space-y-2 overflow-hidden"
+          >
+            <p>Are you sure?</p>
+            <div className="flex items-center justify-center space-x-3">
+              <Button onClick={() => setIsOpen(false)}>No</Button>
+              <Button onClick={endGameOnClick} variant="destructive">
+                Yes
+              </Button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
 
