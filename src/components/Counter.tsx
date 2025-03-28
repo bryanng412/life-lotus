302f99f5ac75fd1765e9f08752a2bb10ca372e00
bryanng <bryanng412@gmail.com>
2025-03-27 22:10:36 -0700

    refactor long press

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,85 +1,23 @@
 import CounterIcon from '@/components/CounterIcon'
+import { useLongPress } from '@/lib/hooks/useLongPress'
 import { usePlayerBoxContext } from '@/lib/hooks/usePlayerBoxContext'
 import { useBoundStore } from '@/lib/store/boundStore'
 import { CounterName } from '@/lib/store/playersSlice'
 import { cn } from '@/lib/utils'
-import { RefObject, useEffect, useState } from 'react'
-import { useInterval } from 'react-interval-hook'
-import { useLongPress } from 'use-long-press'
 
-const LONGPRESS_INTERVAL = 600
-
-const Counter = ({
-  name,
-  value,
-  containerRef,
-}: {
-  name: CounterName
-  value: number
-  containerRef: RefObject<HTMLDivElement | null>
-}) => {
+const Counter = ({ name, value }: { name: CounterName; value: number }) => {
   const { updatePlayerCounter, numPlayers } = useBoundStore()
   const { id } = usePlayerBoxContext()
-  const [incButtonActive, setIncButtonActive] = useState(false)
-  const [decButtonActive, setDecButtonActive] = useState(false)
-  const { start: incIntervalStart, stop: incIntervalStop } = useInterval(
-    () => updatePlayerCounter(id, name, 10),
-    LONGPRESS_INTERVAL,
-    {
-      autoStart: false,
-    }
-  )
-  const { start: decIntervalStart, stop: decIntervalStop } = useInterval(
-    () => updatePlayerCounter(id, name, -10),
-    LONGPRESS_INTERVAL,
-    {
-      autoStart: false,
-    }
-  )
 
-  const addBind = useLongPress(incIntervalStart, {
-    threshold: LONGPRESS_INTERVAL,
-    onStart: () => setIncButtonActive(true),
-    onFinish: () => {
-      incIntervalStop()
-      setIncButtonActive(false)
-    },
-    onCancel: () => {
-      updatePlayerCounter(id, name, 1)
-      incIntervalStop()
-      setIncButtonActive(false)
-    },
-    cancelOnMovement: false,
-    cancelOutsideElement: false,
-  })
-  const subtractBind = useLongPress(decIntervalStart, {
-    threshold: LONGPRESS_INTERVAL,
-    onStart: () => setDecButtonActive(true),
-    onFinish: () => {
-      decIntervalStop()
-      setDecButtonActive(false)
-    },
-    onCancel: () => {
-      updatePlayerCounter(id, name, -1)
-      decIntervalStop()
-      setDecButtonActive(false)
-    },
-    cancelOnMovement: false,
-    cancelOutsideElement: false,
+  const incrementProps = useLongPress({
+    onPress: () => updatePlayerCounter(id, name, 1),
+    onLongPress: () => updatePlayerCounter(id, name, 10),
   })
 
-  useEffect(() => {
-    const handleScroll = () => {
-      incIntervalStop()
-      decIntervalStop()
-    }
-    if (containerRef.current) {
-      containerRef.current.addEventListener('scroll', handleScroll)
-      const refCopy = containerRef.current
-
-      return () => refCopy.removeEventListener('scroll', handleScroll)
-    }
-  }, [incIntervalStop, decIntervalStop, containerRef])
+  const decrementProps = useLongPress({
+    onPress: () => updatePlayerCounter(id, name, -1),
+    onLongPress: () => updatePlayerCounter(id, name, -10),
+  })
 
   return (
     <div className="relative flex size-full flex-col items-center justify-center not-first:border-l-1 md:justify-evenly">
@@ -89,18 +27,18 @@ const Counter = ({
       <CounterIcon className="mt-4 md:mt-0" counterName={name} />
       <div className="absolute flex size-full flex-col">
         <button
-          {...addBind()}
           className={cn(
             'bg-primary size-full cursor-pointer opacity-0',
-            incButtonActive && 'opacity-8'
+            incrementProps.isPressed && 'opacity-8'
           )}
+          {...incrementProps}
         />
         <button
-          {...subtractBind()}
           className={cn(
             'bg-primary size-full cursor-pointer opacity-0',
-            decButtonActive && 'opacity-5'
+            decrementProps.isPressed && 'opacity-8'
           )}
+          {...decrementProps}
         />
       </div>
     </div>
